af261b9fe0a04a3f3f017a0e7b2f8d4785c14718
Steven <wiggyste@googlemail.com>
2026-04-27 21:14:21 +0100

    Fix build by removing redundant workspace folder

--- workspace/src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-'use server';
-// Flows will be imported for their side effects in this file.
-import './flows/generate-similar-colors-flow';
-import './flows/extract-gradients-flow';
-import './flows/extract-svg-colors-flow';
-import './flows/suggest-color-update-flow';
